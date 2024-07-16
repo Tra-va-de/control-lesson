260d7b66d37751a675c8ab90f6a5a952de890654
Tra-va-de <distraction.mansion@gmail.com>
2024-07-16 20:02:24 +0300

    Увеличили длину поля ответа.

--- app/schemas/question.py
+++ app/schemas/question.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field, ConfigDict
 
 class QuestionBase(BaseModel):
     text: str = Field(..., min_length=1, max_length=100)
-    answer: str = Field(..., min_length=1, max_length=100)
+    answer: str = Field(..., min_length=1, max_length=1000)
     subject_id: int = Field(..., gt=0)
 
 
