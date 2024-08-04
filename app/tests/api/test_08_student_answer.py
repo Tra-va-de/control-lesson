ccfd2c224076b323d56c103f0d08c9750d586040
Tra-va-de <distraction.mansion@gmail.com>
2024-08-04 07:17:53 +0300

    Добавили тесты для новых API.

--- app/tests/api/test_08_student_answer.py
+++ app/tests/api/test_08_student_answer.py
@@ -159,6 +159,46 @@ async def test_create_or_update_student_answer_after_delete(client: AsyncClient,
 
 @pytest.mark.asyncio
 @pytest.mark.order(10)
+async def test_read_student_answers_by_student_id(client: AsyncClient, create_student: int):
+    student_id = create_student
+    print(f"student_id: {student_id}")
+    response = await client.get(f"/api/v1/student-answers/student/{student_id}")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) > 0
+    for student_answer in data:
+        assert student_answer["student_id"] == student_id
+
+
+@pytest.mark.asyncio
+@pytest.mark.order(11)
+async def test_read_student_answers_by_question_id(client: AsyncClient, create_question: int):
+    question_id = create_question
+    print(f"question_id: {question_id}")
+    response = await client.get(f"/api/v1/student-answers/question/{question_id}")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) > 0
+    for student_answer in data:
+        assert student_answer["question_id"] == question_id
+
+
+@pytest.mark.asyncio
+@pytest.mark.order(12)
+async def test_read_student_answer_by_student_and_question_id(client: AsyncClient, create_student: int, create_question: int):
+    student_id = create_student
+    print(f"student_id: {student_id}")
+    question_id = create_question
+    print(f"question_id: {question_id}")
+    response = await client.get(f"/api/v1/student-answers/student-and-question/{student_id}/{question_id}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["student_id"] == student_id
+    assert data["question_id"] == question_id
+
+
+@pytest.mark.asyncio
+@pytest.mark.order(13)
 async def test_get_non_existent_student_answer(client: AsyncClient):
     response = await client.get("/api/v1/student-answers/999")
     print("Response status:", response.status_code)
