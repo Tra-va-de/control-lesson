39a3e32559111e0b9356fc17c532e9b028a4ee06
Tra-va-de <distraction.mansion@gmail.com>
2024-09-02 19:43:23 +0300

    Сделали рендер данных с БД.

--- vue/src/components/ThePersonalQualities.vue
+++ vue/src/components/ThePersonalQualities.vue
@@ -1,13 +1,31 @@
 <script setup>
+	import { ref, onMounted } from 'vue'
+
+	import axios from 'axios'
+
 	import Qualitie from './Qualitie.vue'
+
+	const apiEndpoint = import.meta.env.VITE_PRIVATE_API_ENDPOINT
+	
+	const attitudes = ref([])
+
+	const fetchLearningAttitudes = async () => {
+		try {
+			const response = await axios.get(
+				apiEndpoint + '/learning-attitudes/'
+			)
+			attitudes.value = response.data
+		} catch (error) {
+			console.error('Failed to fetch learning attitudes:', error)
+		}
+	}
+
+	onMounted(fetchLearningAttitudes)
 </script>
 
 <template>
 	<div class="personal-qualities">
-		<Qualitie title="Интерес" />
-		<Qualitie title="Мотивация" />
-		<Qualitie title="Вовлеченность" />
-		<Qualitie title="Успаеваемость" />
+		<Qualitie v-for="attitude in attitudes" :key="attitude.id" :title="attitude.name" :id="attitude.id" />
 	</div>
 </template>
 
