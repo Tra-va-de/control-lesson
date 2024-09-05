a42296ca949d30f5849f862a2ddccf518811a725
Tra-va-de <distraction.mansion@gmail.com>
2024-09-05 16:11:57 +0300

    Сделали получение данных об ученике и уроке из localStorage.

--- vue/src/pages/Home.vue
+++ vue/src/pages/Home.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref, provide } from 'vue'
+    import { ref, provide, onMounted } from 'vue'
 
     import SelectLesson from '@/components/SelectLesson.vue'
     import ControlLesson from '@/components/ControlLesson.vue';
@@ -9,6 +9,27 @@
     
     provide('selectedStudent', selectedStudent)
     provide('selectedLesson', selectedLesson)
+
+    // Функция для получения сохраненных данных
+    const getSelection = (selectionName) => {
+        // Пытаемся получить сохраненные данные
+        const selection = JSON.parse(localStorage.getItem(selectionName));
+        if (selection) {
+            const currentTime = new Date().getTime();
+            // Проверяем, не истек ли час (3600000 мс)
+            if (currentTime - selection.timestamp < 3600000) {
+                return selection.value; // Возвращаем сохраненные данные, если они актуальны
+            } else {
+                localStorage.removeItem(selectionName); // Удаляем устаревшие данные
+            }
+        }
+        return null; // Данные не найдены или устарели
+    };
+
+    onMounted(() => {
+        selectedStudent.value = getSelection('selectedStudent');
+        selectedLesson.value = getSelection('selectedLesson');
+    });
 </script>
 
 <template>
