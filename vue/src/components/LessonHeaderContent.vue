0e99a1315baa9a1a7c02b583ef909f46f64b497e
Tra-va-de <distraction.mansion@gmail.com>
2024-09-05 17:46:21 +0300

    Сделали кнопку сброса выбранного студента.

--- vue/src/components/LessonHeaderContent.vue
+++ vue/src/components/LessonHeaderContent.vue
@@ -1,108 +1,132 @@
 <script setup>
-	import { ref, watch } from 'vue';
-	import axios from 'axios'
-	
-	const apiEndpoint = import.meta.env.VITE_PRIVATE_API_ENDPOINT;
+import { ref, watch, inject } from 'vue'
+import axios from 'axios'
 
-	const props = defineProps({
-		lessonId: Number,
-		studentName: String,
-		lessonsLassed: Number,
-		initialDate: String
-	})
+const apiEndpoint = import.meta.env.VITE_PRIVATE_API_ENDPOINT
 
-	const lessonName = ref('');
+const props = defineProps({
+  lessonId: Number,
+  studentName: String,
+  lessonsLassed: Number,
+  initialDate: String
+})
 
-	const fetchLessonName = async (lessonId) => {
-		try {
-			const lessonDetails = await axios.get(
-				`${apiEndpoint}/lessons/${lessonId}`
-			)
-			const disciplineDetails = await axios.get(
-				`${apiEndpoint}/disciplines/${lessonDetails.data.discipline_id}`
-			)
-			const levelDetails = await axios.get(
-				`${apiEndpoint}/lesson-levels/${lessonDetails.data.lesson_level_id}`
-			)
+const selectedStudent = inject('selectedStudent')
 
-			lessonName.value = `${disciplineDetails.data.name}. ${levelDetails.data.name} уровень. ${lessonDetails.data.name}`
-		} catch (error) {
-			console.log(error)
-		}
-	}
+const lessonName = ref('')
 
-	watch(
-		() => props.lessonId,
-		async (newLessonId) => {
-			if (newLessonId) {
-				await fetchLessonName(newLessonId);
-			}
-		},
-		{ immediate: true }
-	);
+const fetchLessonName = async (lessonId) => {
+  try {
+    const lessonDetails = await axios.get(`${apiEndpoint}/lessons/${lessonId}`)
+    const disciplineDetails = await axios.get(
+      `${apiEndpoint}/disciplines/${lessonDetails.data.discipline_id}`
+    )
+    const levelDetails = await axios.get(
+      `${apiEndpoint}/lesson-levels/${lessonDetails.data.lesson_level_id}`
+    )
 
-	const formatDate = (date) => {
-		const d = new Date(date);
-		const day = String(d.getDate()).padStart(2, '0');
-		const month = String(d.getMonth() + 1).padStart(2, '0');
-		const year = d.getFullYear();
+    lessonName.value = `${disciplineDetails.data.name}. ${levelDetails.data.name} уровень. ${lessonDetails.data.name}`
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-		return `${day}.${month}.${year}`;
-	}
+watch(
+  () => props.lessonId,
+  async (newLessonId) => {
+    if (newLessonId) {
+      await fetchLessonName(newLessonId)
+    }
+  },
+  { immediate: true }
+)
 
-	const date = ref(props.initialDate);
-	const isHovered = ref(false);
+const formatDate = (date) => {
+  const d = new Date(date)
+  const day = String(d.getDate()).padStart(2, '0')
+  const month = String(d.getMonth() + 1).padStart(2, '0')
+  const year = d.getFullYear()
 
-	const updateDate = () => {
-		// Выводим новую дату в формате "дд.мм.гггг"
-		date.value = formatDate(new Date());
-	};
+  return `${day}.${month}.${year}`
+}
+
+const date = ref(props.initialDate)
+const isHoveredStudent = ref(false)
+const isHoveredDate = ref(false)
+
+const unselectStudent = () => {
+  selectedStudent.value = null
+}
+
+const updateDate = () => {
+  // Выводим новую дату в формате "дд.мм.гггг"
+  date.value = formatDate(new Date())
+}
 </script>
 
 <template>
-	<h1 class="lesson-name">{{ lessonName }}</h1>
+  <h1 class="lesson-name">{{ lessonName }}</h1>
 
-	<ul class="student-info">
-		<li class="student-name">Ученик: {{ studentName }}</li>
-		<li class="lessons-liassed">Уроков пройдено: {{ lessonsLassed }}</li>
-		<li class="date-of-lesson"
-			@mouseenter="isHovered = true" 
-			@mouseleave="isHovered = false">
-			<p class="date" v-auto-animate>Дата: {{ date }}</p>
-			<button v-if="isHovered" @click="updateDate" class="update-date-btn">
-				✓
-			</button>
-		</li>
-	</ul>
+  <ul class="student-info">
+    <li
+      class="student-name"
+      @mouseenter="isHoveredStudent = true"
+      @mouseleave="isHoveredStudent = false"
+      v-auto-animate
+    >
+      <p class="student">Ученик: {{ studentName }}</p>
+      <button v-if="isHoveredStudent" @click="unselectStudent" class="unselect-student-btn">
+        ✕
+      </button>
+    </li>
+    <li class="lessons-liassed">Уроков пройдено: {{ lessonsLassed }}</li>
+    <li
+      class="date-of-lesson"
+      @mouseenter="isHoveredDate = true"
+      @mouseleave="isHoveredDate = false"
+      v-auto-animate
+    >
+      <p class="date" v-auto-animate>Дата: {{ date }}</p>
+      <button v-if="isHoveredDate" @click="updateDate" class="update-date-btn">✓</button>
+    </li>
+  </ul>
 </template>
 
 <style scoped lang="scss">
-	.lesson-name {
-		font-size: 22px;
-		font-weight: 500;
-		padding-bottom: 20px;
-	}
+.lesson-name {
+  font-size: 22px;
+  font-weight: 500;
+  padding-bottom: 20px;
+}
 
-	.student-info li {
-		font-size: 17px;
-		line-height: 24px;
+.student-info li {
+  font-size: 17px;
+  line-height: 24px;
 
-		display: flex;
-		align-items: center;
-		column-gap: 10px;
-	}
+  display: flex;
+  align-items: center;
+  column-gap: 10px;
+}
 
-	.update-date-btn {
-		background-color: green;
-		color: white;
+.unselect-student-btn,
+.update-date-btn {
+  color: white;
 
-		border: none;
-		border-radius: 50%;
-		
-		width: 24px;
-		height: 24px;
-		
-		cursor: pointer;
-		font-size: 14px;
-	}
+  border: none;
+  border-radius: 50%;
+
+  width: 24px;
+  height: 24px;
+
+  cursor: pointer;
+  font-size: 14px;
+}
+
+.unselect-student-btn {
+  background-color: red;
+}
+
+.update-date-btn {
+  background-color: green;
+}
 </style>
